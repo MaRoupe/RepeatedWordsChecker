860a7ff0c91f967ebe698671bdd926bf8460927b
Martin Roupé <19404726+MaRoupe@users.noreply.github.com>
2021-02-06 13:14:32 +0100

    Initial commit

---
+++ RepeatedWordsChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RepeatedWordsChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Take the sentence and separate the words.
+
+            string textString = "";
+
+            while ((textString = Console.ReadLine()) != null)
+            {
+                string[] wordSplit = textString.Split(" ");
+
+                //Check if any of the words are repeated.
+                string testAnswer = "";
+
+                testAnswer = (!wordSplit.All(new HashSet<string>().Add)) ? "no" : "yes";
+
+                // Print Yes or No depending on if a word is repeated.
+                Console.WriteLine(testAnswer);
+            }
+        }
+    }
+}
+}
